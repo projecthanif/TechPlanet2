457668736741ec95e6cc503c3cd3db7f07f56bdb
projecthanif <iamustapha213@gmail.com>
2024-07-20 04:00:20 +0100

    :feat: this works by laveraging the use of session helper that laravel provided for us

--- app/Actions/Order/StoreOrderAction.php
+++ app/Actions/Order/StoreOrderAction.php
@@ -3,34 +3,43 @@
 namespace App\Actions\Order;
 
 use App\Models\Cart;
+use App\Models\CartItem;
 use App\Models\Product;
 
 class StoreOrderAction
 {
     public function addOrder(Product $product)
     {
+        $this->guest($product);
     }
 
-    public function guest(int $productId)
+    private function guest(Product $product)
     {
         $id = Cart::where([
             'session_id' => session()->id()
-        ])->get('id')->first()->id;
-
+        ])->get('id')->first()?->id;
 
         if ($id === null) {
             $session = Cart::create([
                 'session_id' => session()->id()
             ]);
-
             $id = $session->id;
         }
 
-        dd($id);
+        $quantity = 1;
+
+        $cart = CartItem::create([
+            'cart_id' => $id,
+            'product_id' => $product->id,
+            'quantity' => $quantity,
+            'price' => $product->price * $quantity
+        ]);
+
+//        dump($cart);
 
     }
 
-    public function authUser()
+    private function authUser()
     {
 
     }
--- app/Livewire/Shop.php
+++ app/Livewire/Shop.php
@@ -20,21 +20,6 @@ class Shop extends Component
     }
     public function render()
     {
-        $id = Cart::where([
-            'session_id' => session()->id()
-        ])->get('id')->first()?->id;
-
-
-        if ($id === null) {
-            $session = Cart::create([
-                'session_id' => session()->id()
-            ]);
-
-            $id = $session->id;
-        }
-
-        dd($id);
-
         $product = Product::paginate(12);
         return view('livewire.shop', [
             'products' => $product
